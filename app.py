eaad23c045bbececea1298ad77d44441c5549044
alexbanuelosvielmas <alba7616@colorado.edu>
2026-07-26 18:45:35 +0000

    Customize Flask root route

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route("/")
+def index():
+    return "Hello World from Alex Banuelos in 3308"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
